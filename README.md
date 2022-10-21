d825cd58a5fa1e3c2cea608455db8dcff1e600c2
Shubhro Saha <shbhrsaha@gmail.com>
2022-10-21 18:27:05 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# wikipedia-gpt3-bot
+# Wikipedia GPT-3 Bot
+
 REPL bot that answers questions by querying Wikipedia and summarizing answers with GPT-3
+
+# Setup
+
+```bash
+export OPENAI_API_KEY=<PASTE YOUR OPENAI API KEY HERE>
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+# Usage
+
+```
+> python main.py
+
+Welcome to Wikipedia GPT-3 Bot. Ask any question.
+
+(wikibot) Who made Stable Diffusion?
+Pulling up page: Stable Diffusion
+
+
+    URL: https://en.wikipedia.org/wiki/Stable_Diffusion
+
+    Context:
+
+    > [...]  detailed images conditioned on text descriptions, though it can also be applied to other tasks such as inpainting, outpainting, and generating image-to-image translations guided by a text prompt.Stable Diffusion is a latent diffusion model, a variety of deep generative neural network developed by the CompVis group at LMU Munich. The model has been released by a collaboration of Stability AI, CompVis LMU, and Runway with support from EleutherAI and LAION. In October 2022, Stability AI raised $101 million dollars in a roun  [...]
+
+    Answer: Stable Diffusion was made by a collaboration of Stability AI, CompVis LMU, and Runway with support from EleutherAI and LAION.
+
+
+(wikibot) When did Liz Truss resign?
+Pulling up page: Liz Truss
+
+
+    URL: https://en.wikipedia.org/wiki/Liz_Truss
+
+    Context:
+
+    > [...] lic sector organisations. Her government announced large-scale borrowing and various tax cuts in a mini-budget, which led to financial instability, was widely criticised and was largely reversed. On 20 October 2022, amidst economic and political crisis, Truss announced her intention to resign as prime minister after 44 days in office, making her the shortest serving prime minister in the history of the UK. [...] of plans to cut taxes for people earning over £50,000. Consequently, it was thought she would be appointed chancellor of the Exchequer or business secretary, but she was instead promoted to  [...]
+
+    Answer: Liz Truss resigned on 20 October 2022.
+
+
+(wikibot) How big is the iPhone 14?
+Pulling up page: IPhone 14
+
+
+    URL: https://en.wikipedia.org/wiki/IPhone_14
+
+    Context:
+
+    > [...] 3 and iPhone 13 Mini, and were announced at the Apple Event in Apple Park in Cupertino, California on September 7, 2022, alongside the higher-priced iPhone 14 Pro and iPhone 14 Pro Max flagships. The iPhone 14 and iPhone 14 Plus feature a 6.1-inch (15 cm) and 6.7-inch (17 cm) display, improvements to the rear-facing camera, and satellite connectivity. The iPhone 14 was made available on September 16, 2022 and iPhone 14 Plus was made available on October 7, 2022 respectively,   [...]
+
+    Answer: The iPhone 14 has a 6.1-inch display.
+
+(wikibot)
+```
---
+++ main.py
@@ -0,0 +1,182 @@
+
+import sys
+import os
+import openai
+import wikipedia
+import re
+import cmd
+
+OPEN_API_KEY_ENV_VAR = "OPENAI_API_KEY"
+
+MODEL = "text-davinci-002"
+TEMPERATURE = 0.5
+MAX_TOKENS = 256
+
+CONTEXT_CHAR_LIMIT = 4000
+CONTEXT_HALF_SIZE = 300
+EXCERPT_HALF_SIZE = 200
+
+CONTEXT_DIVIDER = " [...] "
+BOLD_SEQUENCE_START = "\033[1m"
+BOLD_SEQUENCE_END = "\033[0m"
+
+
+def ensure_env_vars():
+    if OPEN_API_KEY_ENV_VAR not in os.environ:
+        print(f"{OPEN_API_KEY_ENV_VAR} not found in environment variables")
+        sys.exit(1)
+
+
+def gen_completion(prompt):
+    response = openai.Completion.create(
+        model=MODEL,
+        prompt=prompt,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS,
+    )
+
+    return sanitize_response(response.choices[0].text)
+
+
+def sanitize_response(response):
+    return response.strip().replace("\n", "").replace("\"", "")
+
+
+def question_to_page_search_query(question):
+    prompt = f"""
+Which Wikipedia page would you search for to answer the following question: "{question}"
+
+Page name:"""
+
+    return gen_completion(prompt)
+
+
+def page_search_query_to_page(page_search_query):
+    search_results = wikipedia.search(page_search_query)
+
+    if len(search_results) == 0:
+        print("No Wikipedia pages found to answer the question")
+        sys.exit(1)
+
+    try:
+        return wikipedia.page(search_results[0], auto_suggest=False)
+    except wikipedia.DisambiguationError as e:
+        return wikipedia.page(e.options[0], auto_suggest=False)
+
+
+def generate_ctrlf_term(page, question):
+    prompt = f"""
+Given the Wikipedia page for "{page.title}", what word would you search for on the page to answer the question "{question}"
+
+Search term:"""
+
+    return gen_completion(prompt)
+
+
+def generate_context(page, ctrlf_term):
+    matches = [match.start() for match in re.finditer(
+        ctrlf_term, page.content, re.IGNORECASE)]
+
+    top_three_contexts = []
+    for match in matches:
+        if len(top_three_contexts) == 3:
+            break
+
+        excerpt = page.content[match - CONTEXT_HALF_SIZE: match +
+                               CONTEXT_HALF_SIZE] if match > CONTEXT_HALF_SIZE else page.content[0: match + match]
+
+        if excerpt in page.summary:
+            continue
+
+        top_three_contexts.append(excerpt.strip())
+
+    return (CONTEXT_DIVIDER.join([page.summary.strip()] +
+                                 top_three_contexts))[:CONTEXT_CHAR_LIMIT]
+
+
+def generate_answer(context, question):
+    prompt = f"""
+Background text: 
+
+"{context}"
+
+Answer the following question using only the background text above.
+
+Question: "{question}"
+Answer:"""
+
+    return gen_completion(prompt)
+
+
+def generate_excerpt(context, question):
+    prompt = f"""
+Background text: 
+
+"{context}"
+
+Given the background text above, which substring would you highlight to answer the question "{question}"
+
+Excerpt:"""
+
+    excerpt = gen_completion(prompt)
+
+    if excerpt in context:
+        return excerpt
+    else:
+        return None
+
+
+def generate_context_highlighted(context, excerpt):
+    if excerpt is None or excerpt not in context:
+        return context.replace("\n", " ")
+
+    context_highlighted = context.replace(
+        excerpt, f"{BOLD_SEQUENCE_START}{excerpt}{BOLD_SEQUENCE_END}") if excerpt else context
+
+    match = context_highlighted.find(excerpt)
+    context_highlighted_abridged = context_highlighted[match - EXCERPT_HALF_SIZE: match + len(
+        excerpt) + EXCERPT_HALF_SIZE] if match > EXCERPT_HALF_SIZE else context_highlighted[0: match + len(excerpt) + EXCERPT_HALF_SIZE]
+    return context_highlighted_abridged.replace("\n", " ")
+
+
+def answer_question(question):
+    page_search_query = question_to_page_search_query(question)
+
+    page = page_search_query_to_page(page_search_query)
+    print(f"Pulling up page: {page.title}")
+
+    ctrlf_term = generate_ctrlf_term(page, question)
+
+    context = generate_context(page, ctrlf_term)
+
+    answer = generate_answer(context, question)
+    excerpt = generate_excerpt(context, question)
+
+    context_highlighted = generate_context_highlighted(context, excerpt)
+
+    summary_pretty = f"""
+
+    URL: {page.url}
+
+    Context:
+
+    > [...] {context_highlighted}  [...]
+
+    Answer: {BOLD_SEQUENCE_START}{answer}{BOLD_SEQUENCE_END}
+
+    """
+
+    print(summary_pretty)
+
+
+class TurtleShell(cmd.Cmd):
+    intro = '\nWelcome to Wikipedia GPT-3 Bot. Ask any question.\n'
+    prompt = '(wikibot) '
+
+    def default(self, line):
+        answer_question(line)
+
+
+if __name__ == '__main__':
+    ensure_env_vars()
+    TurtleShell().cmdloop()
